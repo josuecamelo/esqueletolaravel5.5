b252469d5d4442ae614699fe8d38109e685dffe2
Josué Camelo <josueprg@gmail.com>
2017-12-23 14:35:26 -0200

    Refresh Token

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,35 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1', 'middleware' => 'cors'], function () {
+/*Route::group(['prefix' => 'v1', 'middleware' => 'cors'], function () {
     Route::group(['middleware' => 'jwt.auth'], function () {
         //Route::get('products', 'Api\ProductsController@index');
         //Route::get('session', 'Api\PagSeguroController@getSessionId');
         //Route::post('order', 'Api\OrdersController@store');
     });
     Route::post('login', 'Api\AuthController@login');
-});
+});*/
 
+Route::group(['middleware' => 'cors'], function () {
+    Route::group(['middleware' => 'jwt.auth'], function () {
+        //Route::get('products', 'Api\ProductsController@index');
+        //Route::get('session', 'Api\PagSeguroController@getSessionId');
+        //Route::post('order', 'Api\OrdersController@store');
+        Route::get('user', function () {
+            $user = JWTAuth::parseToken()->toUser();
+
+            return response()->json(compact('user'));
+        });
+    });
+    Route::post('login', 'Api\AuthController@login');
+
+    //para atualizar o token
+    Route::post('refresh_token', function(){
+        try {
+            $token = JWTAuth::parseToken()->refresh();
+            return response()->json(compact('token'));
+        }catch (JWTException $exception){
+            return response()->json(['error' => 'token_invalid'],400);
+        }
+    });
+});
